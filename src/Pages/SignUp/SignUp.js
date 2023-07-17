8234e78937f3b995d54c587a3cdc7d623d866ff7
Mehnaz <mehnazchowdhury613@gmail.com>
2023-07-17 13:57:42 +0600

    second

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider';
 
 const SignUp = () => {
-    const { register,formState: { errors }, handleSubmit } = useForm();
+    const { register, handleSubmit,formState: { errors } } = useForm();
+    const {createUser}=useContext(AuthContext);
   
-    const handleSignUp =data=>{
+    const handleSignUp =(data)=>{
        console.log(data)
+       createUser(data.email,data.password)
+       .then(result=>{
+        const user= result.user;
+        console.log(user)
+       })
+       .catch(error=>console.log(error))
     }
     return (
         <div className="h-[600px] flex justify-center items-center">
